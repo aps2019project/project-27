d8d9f09ef4216d73dd910ab321d2658778afb97a
amm266 <a.m.mohammadi@gmail.com>
2019-04-14 10:20:41 +0430

    make class spell and add stats

--- src/Spell.java
+++ src/Spell.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+
 public class Spell extends Card {
+	private ArrayList<Spell> spells = new ArrayList<Spell> (  );
 	private String[] target;
 	private String[] effects;
 }
