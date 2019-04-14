0ed6de3b575a648ac4d731efc204cbc6fb4d7f33
amm266 <a.m.mohammadi@gmail.com>
2019-04-14 10:25:49 +0430

    make class minionAndHero and stats

--- src/MinionAndHero.java
+++ src/MinionAndHero.java
@@ -1,3 +1,11 @@
-public class MinionAndHero {
+import java.util.ArrayList;
 
+public class MinionAndHero extends Card {
+	private ArrayList<MinionAndHero> minionAndHeroes = new ArrayList<> (  );
+	private int AP;
+	private int HP;
+	private String specialPower;
+	private int nation;
+	private int attackType;
+	private String[] abilities;
 }
