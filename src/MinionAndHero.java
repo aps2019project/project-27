be215ced52850bf334d50531808fef06e5d01722
amm266 <a.m.mohammadi@gmail.com>
2019-04-15 10:10:47 +0430

    add stats and get methods to M&H

--- src/MinionAndHero.java
+++ src/MinionAndHero.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
 
 public class MinionAndHero extends Card {
-	private ArrayList<MinionAndHero> minionAndHeroes = new ArrayList<> (  );
+	private  static ArrayList<MinionAndHero> minionAndHeroes = new ArrayList<> (  );
+	private boolean isHero;
 	private int AP;
 	private int HP;
 	private String specialPower;
 	private int nation;
 	private int attackType;
 	private String[] abilities;
+	public int getNation () {
+		return nation;
+	}
+	public boolean isHero () {
+		return isHero;
+	}
+	public String[] getAbilities(){
+		return abilities;
+	}
 }
