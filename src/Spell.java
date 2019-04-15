5f911f2ef8e4d6859574b92d5a3e17a7ca73f4ae
amm266 <a.m.mohammadi@gmail.com>
2019-04-15 22:49:25 +0430

    del load.java

--- src/Spell.java
+++ src/Spell.java
@@ -4,4 +4,8 @@ public class Spell extends Card {
 	private ArrayList<Spell> spells = new ArrayList<Spell> (  );
 	private String[] target;
 	private String[] effects;
+
+	protected Spell ( String name , int cardID , int shopPrice , int manaPrice ) {
+		super ( name , cardID , shopPrice , manaPrice );
+	}
 }
