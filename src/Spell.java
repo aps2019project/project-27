900e831ef7dee08fbf398f1a7b0d543d7bca1fca
hesan <hesanghasemi99@gmail.com>
2019-04-16 16:54:51 +0430

    deleted x

--- src/Spell.java
+++ src/Spell.java
@@ -4,7 +4,6 @@ public class Spell extends Card {
 	private ArrayList<Spell> spells = new ArrayList<Spell> (  );
 	private String[] target;
 	private String[] effects;
-	private int x;
 
 	protected Spell ( String name , int cardID , int shopPrice , int manaPrice ) {
 		super ( name , cardID , shopPrice , manaPrice );
