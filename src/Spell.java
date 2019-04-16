96f167918e98ba324acc7f1f953214bf5767d410
hesan <hesanghasemi99@gmail.com>
2019-04-16 16:53:11 +0430

    added x check commit

--- src/Spell.java
+++ src/Spell.java
@@ -4,6 +4,7 @@ public class Spell extends Card {
 	private ArrayList<Spell> spells = new ArrayList<Spell> (  );
 	private String[] target;
 	private String[] effects;
+	private int x;
 
 	protected Spell ( String name , int cardID , int shopPrice , int manaPrice ) {
 		super ( name , cardID , shopPrice , manaPrice );
