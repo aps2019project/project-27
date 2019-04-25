fece388fa3492825143dbf98cc36a3199c23c8f4
amm266 <a.m.mohammadi@gmail.com>
2019-04-25 16:01:38 +0430

    create buff

---
+++ src/Moudle/Buff.java
@@ -0,0 +1,19 @@
+package Moudle;
+
+public class Buff {
+	private String name;
+	private boolean isPositive;
+	private int changeAP;
+	private int changeHP;
+	private int changeHollynes;
+	private boolean changeCanMove;
+	private boolean changeCanAttack;
+	private boolean changeCanCounterAttack;
+	private boolean disableEnemyPositiveBuffs;
+	private boolean disablePlayerNegativeBuffs;
+	private int ageType;
+	private int age;
+	private Buff cellBuff;
+
+
+}
--- src/Moudle/Fighter.java
+++ src/Moudle/Fighter.java
@@ -1,12 +1,18 @@
 package Moudle;
 
+import java.util.ArrayList;
+
 public class Fighter extends Card {
 	private int AP;
 	private int HP;
 	private int x;
 	private int y;
+	ArrayList<Buff> buffs = new ArrayList<> (  );
 	private Cell currentCell;
 	private Player player;
+	public void addToBuff(Buff buff){
+
+	}
 	protected Fighter ( String name , int cardID , int shopPrice , int manaPrice ) {
 		super ( name , cardID , shopPrice , manaPrice );
 
