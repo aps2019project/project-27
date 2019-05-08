cb12b3f06251b9dcc6aab2b96d735aa1f5f7b9c5
hesan <hesanghasemi99@gmail.com>
2019-05-08 17:36:10 +0430

    what's up1

--- src/Moudle/Account.java
+++ src/Moudle/Account.java
@@ -3,6 +3,7 @@ package Moudle;
 import Controller.ControlBox;
 import Controller.Controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -23,12 +24,12 @@ public class Account {
         return mainAccount;
     }
 
-    public static ArrayList<Account> getAccounts() {
-        return accounts;
-    }
-
     public static void setAccounts(ArrayList<Account> accounts) {
         Account.accounts = accounts;
+    }
+
+    public static ArrayList<Account> getAccounts() {
+        return accounts;
     }
 
     public static int input(ControlBox controlBox) {
@@ -75,7 +76,7 @@ public class Account {
             System.out.println("There is an account with this userName!");
         } else {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter passWord!");
+            System.out.println("Enter password:");
             String passWord = scanner.next();
             Account account = new Account();
             account.userName = userName;
@@ -91,14 +92,14 @@ public class Account {
             System.out.println("There is no account with this userName!");
             return false;
         } else {
-            System.out.println("Enter passWord!");
+            System.out.println("Enter password:");
             Scanner scanner = new Scanner(System.in);
             String passWord = scanner.next();
             if (passWord.equals(findAccount(userName).passWord)) {
                 System.out.println("login successful!");
                 return true;
             } else {
-                System.out.println("Wrong passWord!");
+                System.out.println("Wrong password!");
                 return false;
             }
         }
@@ -118,7 +119,11 @@ public class Account {
     }
 
     public static void save() {
-        //todo
+        try {
+            Load.saveAccount();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void logout() {
