94a62151fadf03dc7188099532c80e0be8517e21
amm266 <a.m.mohammadi@gmail.com>
2019-06-22 19:02:40 +0430

    add battle menu

--- src/View/MainMenufxml.java
+++ src/View/MainMenufxml.java
@@ -6,72 +6,72 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenufxml implements Initializable {
-    public Button Account;
-    public Button CostomeCard;
-    public Button Battle;
-    public Button Shop;
-    public Button Exit;
-    public Button Collection;
+	public Button Account;
+	public Button CostomeCard;
+	public Button Battle;
+	public Button Shop;
+	public Button Exit;
+	public Button Collection;
 	public Label AccountLable;
-    public Button Help;
+	public Button Help;
 
-    @Override
+	@Override
 	public void initialize ( URL location , ResourceBundle resources ) {
-		Moudle.Account account = Moudle.Account.getMainAccount ();
-		if ( account !=null ){
-			AccountLable.setText ( account.getUserName () );
+		Moudle.Account account = Moudle.Account.getMainAccount ( );
+		if ( account != null ) {
+			AccountLable.setText ( account.getUserName ( ) );
 		}
 		AnimationTimer animationTimer = new AnimationTimer ( ) {
-    public void initialize(URL location, ResourceBundle resources) {
-        AnimationTimer animationTimer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                Account.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        Graphic.setRegion("Account");
-                    }
-				}
+			@Override
+			public void handle ( long now ) {
+				Account.setOnAction ( new EventHandler<ActionEvent> ( ) {
+										  @Override
+										  public void handle ( ActionEvent event ) {
+											  Graphic.setRegion ( "Account" );
+										  }
+									  }
 				);
+				Help.setOnAction ( new EventHandler<ActionEvent> ( ) {
+					@Override
+					public void handle ( ActionEvent event ) {
+						Graphic.setRegion ( "HelpMenu" );
+					}
+				} );
+				Collection.setOnAction ( new EventHandler<ActionEvent> ( ) {
+					@Override
+					public void handle ( ActionEvent event ) {
+						Graphic.setRegion ( "Collection" );
+					}
+				} );
+				Shop.setOnAction ( new EventHandler<ActionEvent> ( ) {
+					@Override
+					public void handle ( ActionEvent event ) {
+						Graphic.setRegion ( "Shop" );
+					}
+				} );
 				Battle.setOnAction ( new EventHandler<ActionEvent> ( ) {
 					@Override
 					public void handle ( ActionEvent event ) {
 						Graphic.setRegion ( "BattleMenu" );
 					}
 				} );
-                });
-                Help.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        Graphic.setRegion("HelpMenu");
-                    }
-                });
-                Collection.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        Graphic.setRegion("Collection");
-                    }
-                });
-                Shop.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        Graphic.setRegion("Shop");
-                    }
-                });
-            }
-        };
-        animationTimer.start();
-    }
+			}
+		};
+		animationTimer.start ( );
+	}
 
-    @FXML
-    private void closeButtonAction() {
-        Stage stage = (Stage) Exit.getScene().getWindow();
-        stage.close();
-    }
+	;
+
+	@FXML
+	private void closeButtonAction () {
+		Stage stage = ( Stage ) Exit.getScene ( ).getWindow ( );
+		stage.close ( );
+	}
 }
